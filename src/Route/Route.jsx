fa853215c4ea8fb0f8c21030966f4f8e2d213c95
abdurrahim220 <z.abdurrahim5@gmail.com>
2024-02-09 20:39:48 +0600

    Layout and route created

--- src/Route/Route.jsx
+++ src/Route/Route.jsx
@@ -1,9 +1,17 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import LoadingMain from "../components/LoadingMain";
+import Error from "../pages/Error/Error";
+const Main = React.lazy(() => import("../Layout/Main"));
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <div>Hello world!</div>,
+    errorElement: <Error />,
+    element: (
+      <Suspense fallback={<LoadingMain />}>
+        <Main />
+      </Suspense>
+    ),
   },
 ]);
 
